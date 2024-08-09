ded9e689a4a20d5c151ae613ba811fae4bca4ec4
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2024-08-09 18:09:19 +0100

    updated

---
+++ example/android/app/src/main/kotlin/co/paystack/example/MainActivity.kt
@@ -0,0 +1,5 @@
+package co.paystack.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- example/android/app/src/main/kotlin/co/paystack/flutterpaystackexample/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package co.paystack.flutterpaystackexample
-
-import io.flutter.embedding.android.FlutterActivity
-
-
-class MainActivity: FlutterActivity()
